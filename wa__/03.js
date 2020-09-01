a30a87d4e5babe0cadb124caf2f0243790bc75b0
nhsKK <lightsiber66@gmail.com>
2020-09-01 11:46:46 +0900

    移動、整理

--- wa__/03.js
+++
@@ -1,8 +0,0 @@
-function body__loader(){
-	alert("読み込み完了");
-}
-function form__submit(){
-	var input__ele = document.getElementById("submit__txt");
-	var txt = input__ele.value;
-	return confirm( txt + 'を送信しますか？' );
-}
--- wa__/wa__04/04__02/js/index.js
+++
@@ -1,3 +0,0 @@
-const last__name = $("#last__name").text();
-alert(last__name);
-$("body").append('<p>29歳</p>');
